00f84c74eabc28052b37455ab84bfd9c8019da77
OlegRakovitch <OlegRakovitch@users.noreply.github.com>
2022-05-21 21:18:29 +0000

    Created solution from library template

---
+++ WebSocketCommunication.Memory/DependencyInjectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Staticsoft.WebSocketCommunication.Memory
+{
+    public static class DependencyInjectionExtensions
+    {
+        
+    }
+}
